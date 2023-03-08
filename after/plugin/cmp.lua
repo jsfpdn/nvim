f56e1369d0420a705ebe46d866fd3846e1ec09d4
Josef Podaný <jsfpdn@gmail.com>
2023-03-08 11:17:19 +0100

    fix: formatting

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -9,10 +9,10 @@ cmp.setup({
     experimental = {native_menu = false, ghost_text = true},
     completion = {completeopt = "menu,menuone,noinsert"},
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({select = true})
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({select = true})
     })
 })
