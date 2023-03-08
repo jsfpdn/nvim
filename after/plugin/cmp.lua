90678f1cb5c5cc07f621ddf87109b649d566fd9f
Josef Podaný <jsfpdn@gmail.com>
2023-03-08 11:16:15 +0100

    fix: select autocomplete hint on <CR>

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -7,5 +7,12 @@ cmp.setup({
         documentation = cmp.config.window.bordered()
     },
     experimental = {native_menu = false, ghost_text = true},
-    completion = {completeopt = "menu,menuone,noinsert"}
+    completion = {completeopt = "menu,menuone,noinsert"},
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({select = true})
+    })
 })
